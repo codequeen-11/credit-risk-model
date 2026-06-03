ee8b27b336ac20a8abd6f752b19294a151f96762
codequeen-11 <ayumeyra11@gmail.com>
2026-06-03 03:22:29 -0900

    implemented rfm matrics k-means clustering and high-risk label

---
+++ src/default_estimator.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+ 
+def create_proxy_target(df, n_clusters=3):
+
+    df = df.copy()
+
+    df["TransactionStartTime"] = pd.to_datetime(
+        df["TransactionStartTime"]
+    )
+
+    snapshot_date = (
+        df["TransactionStartTime"].max()
+        + pd.Timedelta(days=1)
+    )
+
+    rfm = (
+        df.groupby("CustomerId")
+        .agg(
+            Recency=(
+                "TransactionStartTime",
+                lambda x: (
+                    snapshot_date - x.max()
+                ).days
+            ),
+            Frequency=(
+                "TransactionId",
+                "count"
+            ),
+            Monetary=(
+                "Value",
+                "sum"
+            )
+        )
+        .reset_index()
+    )
+
+    scaler = StandardScaler()
+
+    rfm_scaled = scaler.fit_transform(
+        rfm[["Recency", "Frequency", "Monetary"]]
+    )
+
+    kmeans = KMeans(
+        n_clusters=n_clusters,
+        random_state=42,
+        n_init=10
+    )
+
+    rfm["cluster"] = kmeans.fit_predict(
+        rfm_scaled
+    )
+
+    cluster_summary = (
+        rfm.groupby("cluster")
+        [["Recency", "Frequency", "Monetary"]]
+        .mean()
+    )
+
+    high_risk_cluster = (
+        cluster_summary
+        .sort_values(
+            by=["Recency", "Frequency", "Monetary"],
+            ascending=[False, True, True]
+        )
+        .index[0]
+    )
+
+    rfm["is_high_risk"] = (
+        rfm["cluster"] == high_risk_cluster
+    ).astype(int)
+
+    return rfm
